88589f0831b5a8fcb295511a255d91380c9a4131
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-17 17:38:42 +0530

    object spread

--- pre/spread/object.js
+++ pre/spread/object.js
@@ -8,7 +8,9 @@ let obj = {
 
 let obj2 = { ...obj, address: { ...obj.address }, skills: [...obj.skills] };
 obj2.name = "afjal";
-obj2.address.city = "mysuru";
+obj2.address.city = "patna";
+obj2.address.state = "bihar";
+obj2.address.pincode = 111111;
 obj2.skills.push("node.js");
 console.log(obj);
 console.log(obj2);
