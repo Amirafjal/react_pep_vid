d070dd987aff982e4382ce8c4e7989a38c8cd2bf
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-17 18:23:48 +0530

    object spread complete

--- pre/spread/object.js
+++ pre/spread/object.js
@@ -6,10 +6,22 @@ let obj = {
     skills: ["html", "css", "react.js"]
 }
 
-let obj2 = { ...obj, address: { ...obj.address }, skills: [...obj.skills] };
+// let obj2 = { ...obj, address: { ...obj.address }, skills: [...obj.skills] }; // ( to give every object a new spread is called DEEP COPY ).
+
+// obj2.name = "afjal";
+// obj2.address.city = "patna";
+// obj2.address.state = "bihar";
+// obj2.address.pincode = 111111;
+// obj2.skills.push("node.js");
+// console.log(obj);
+// console.log(obj2);
+
+// shortcut for every reference spreading ::
+let obj2 = JSON.parse(JSON.stringify(obj));
 obj2.name = "afjal";
 obj2.address.city = "patna";
 obj2.address.state = "bihar";
+obj2.address.currentPlace = "aara";
 obj2.address.pincode = 111111;
 obj2.skills.push("node.js");
 console.log(obj);
