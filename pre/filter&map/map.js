d2aedf60141ed164d37bc731aff8a63aa66c3ec6
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-20 10:56:12 +0530

    map.js

--- pre/filter&map/map.js
+++ pre/filter&map/map.js
@@ -2,8 +2,15 @@ let arr = [1, 2, 3, 4, 5];
 
 let newArr = [];
 
-for (let i = 0; i < arr.length; i++) {
-    newArr[i] = arr[i] * 2;
-}
+// for (let i = 0; i < arr.length; i++) {
+//     newArr[i] = arr[i] * 2;
+// }
+
+newArr = arr.map((value, idx) => {
+    // console.log(value, idx);   
+    return value * 2;
+})
+
+
 console.log(arr);
 console.log(newArr);
