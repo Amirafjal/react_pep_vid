f5538537b26031d1463c385085c1f95bb070181b
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-21 23:20:06 +0530

    filter

--- pre/filter&map/filter.js
+++ pre/filter&map/filter.js
@@ -2,11 +2,17 @@ let arr = ['apple', 'banana', 'orange', 'watermelon'];
 
 let newArr = [];
 
-for (let i = 0; i < arr.length; i++) {
-    let fruit = arr[i];
-    if (fruit.length > 6) {
-        newArr.push(fruit);
-    }
-}
+// for (let i = 0; i < arr.length; i++) {
+//     let fruit = arr[i];
+//     if (fruit.length > 6) {
+//         newArr.push(fruit);
+//     }
+// }
+
+newArr = arr.filter(function (fruit) {
+    return fruit.length > 6;
+})
+
+
 console.log(arr);
 console.log(newArr);
