55bdf869f681a3d1d7310b8e37cffa157059fa57
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-22 01:41:42 +0530

    windows & self object console

--- pre/this/script.js
+++ pre/this/script.js
@@ -1,12 +1,29 @@
-console.log(this);//window
+// console.log(this);//window
 
-function fn() {
-    console.log(this);//window
-}
-fn();
+// function fn() {
+//     console.log(this);//window
+// }
+// fn();
 
-let obj = {
-    name: "amir afjal",
-    func: fn
-}
-obj.func(); //selfobject
+// let obj = {
+//     name: "amir afjal",
+//     func: fn
+// }
+// obj.func(); //selfobject
+
+// -------------------------------
+
+// function fn() {
+//     console.log(this);
+//     function abc() {
+//         console.log(this);
+//     }
+//     abc();
+
+// }
+
+// let obj = {
+//     name: "amir afjal",
+//     func: fn
+// }
+// obj.func();
